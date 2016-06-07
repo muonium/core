6b4120bca678ef7a8ed62731c7458b9e316209a1
hitoshi54 <dylanclement7@gmail.com>
2016-06-07 10:50:52 +0200

    Login with js
    Login page is working and with js

--- application/controllers/Login.php
+++ application/controllers/Login.php
@@ -1,51 +1,65 @@
 <?php
 	class Login extends Languages {
         
-        private $_error = '';
+        function connectionAction() {
+            // Sleep during 3s to avoid a big number of requests (bruteforce)
+            sleep(3);
+            
+            if(!empty($_POST['mail']) && !empty($_POST['pass']) && !empty($_POST['passphrase'])) {
+                if($_POST['passlength'])
+                {
+                    $newUser = new mUsers();
+                    $newUser->setEmail(urldecode($_POST['mail']));
+                    $newUser->setPassword($_POST['pass']);
+                    $newUser->setPassphrase($_POST['passphrase']);
 
-		function DefaultAction() {
+                    //echo $_POST['pass'].'<br />'.$_POST['passphrase'].'<br />';
+                    
+                    $brute = new AntiBruteforce();
+                    $brute->setFolder(ROOT.DS."tmp");
+                    $brute->setNbMaxAttemptsPerHour(50);
 
-			if(!empty($_POST['mail']) && !empty($_POST['pass']) && !empty($_POST['passphrase'])) {
-                sleep(3);
-				$newUser = new mUsers();
-                $newUser->setEmail($_POST['mail']);
-                $newUser->setPassword(hash('sha512', $_POST['pass']));
-                $newUser->setPassphrase(hash('sha512', $_POST['passphrase']));
-                
-                $brute = new AntiBruteforce();
-                $brute->setFolder(ROOT.DS."tmp");
-                $brute->setNbMaxAttemptsPerHour(50);
-                
-                if(!($id = $newUser->getId())) {
-                    // User doesn't exists - Anti bruteforce with session id
-                    $brute->setSID();
-                    $brute->Control();
-                    $this->_error = $this->txt->Login->{"bruteforceErr".$brute->getError()};
-                }
-                else {
-                    if(!($newUser->Connection())) {
-                        // User exists - Anti bruteforce with user id
-                        $brute->setId($id);
+                    if(!($id = $newUser->getId())) {
+                        // User doesn't exists - Anti bruteforce with session id
+                        $brute->setSID();
                         $brute->Control();
-                        $this->_error = $this->txt->Login->{"bruteforceErr".$brute->getError()};
+                        echo htmlentities($this->txt->Login->{"bruteforceErr".$brute->getError()});
                     }
                     else {
-                        // Connection
-                        $_SESSION['id'] = $id;
-                        
-                        $mUserVal = new mUserValidation();
-                        $mUserVal->setIdUser($id);
-
-                        if(!($mUserVal->getKey())) // Unable to find key - Validation is done
-                            header('Location: '.MVC_ROOT);
+                        if(!($newUser->Connection())) {
+                            // User exists - Anti bruteforce with user id
+                            $brute->setId($id);
+                            $brute->Control();
+                            echo htmlentities($this->txt->Login->{"bruteforceErr".$brute->getError()});
+                        }
                         else {
-                            $_SESSION['validate'] = 1;
-                            header('Location: '.MVC_ROOT.'/Validate');
+                            // Connection
+                            $_SESSION['id'] = $id;
+
+                            $mUserVal = new mUserValidation();
+                            $mUserVal->setIdUser($id);
+
+                            if(!($mUserVal->getKey())) // Unable to find key - Validation is done
+                                echo 'ok@';
+                            else {
+                                $_SESSION['validate'] = 1;
+                                echo 'va@';
+                            }
                         }
                     }
                 }
+                else {
+                    echo htmlentities($this->txt->Register->form);
+                }
 			}
-            
+            else {
+                echo htmlentities($this->txt->Register->form);
+            }
+        }
+
+		function DefaultAction() {
+            if(!empty($_SESSION['id']))
+                header('Location: '.MVC_ROOT.'/Error/Error/404');
             include_once(DIR_VIEW."vLogin.php");
 		}
 	};
