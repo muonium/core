764f848421481444daa44fce18dda04235fea31c
Dylan CLEMENT <dylanclement7@gmail.com>
2016-07-31 12:32:47 +0200

    Updated language.js and global.js
    Get JSON content in js is now available.
    User's interface updated : a box appears when you do a right click, with
    different functions according to area (file, folder or other)

--- public/js/Interface/global.js
+++ public/js/Interface/global.js
@@ -8,8 +8,31 @@
     }
 };*/
 
+/* global.js : User's interface */
+
 var Box;
-var Area = 0;
+var Area = 0; // 0 : desktop div, 1 : file, 2 : folder
+
+function setJSON(arr) {
+    txt = arr;
+}
+
+function getCookie(cname) {
+    var name = cname + "=";
+    var ca = document.cookie.split(';');
+    for(var i = 0; i <ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0)==' ') {
+            c = c.substring(1);
+        }
+        if (c.indexOf(name) == 0) {
+            return c.substring(name.length,c.length);
+        }
+    }
+    return "";
+}
+
+// Box class. Show a div 'box' when user uses right click inside desktop div, close the box when user uses left click
 
 var box = class {
     
@@ -18,16 +41,18 @@ var box = class {
     }
     
     left_click(x, y) {
+        // If the user uses left click inside the 'box'
         if((x > this.x && x < this.x + this.box_div.clientWidth) && (y > this.y && y < this.y + this.box_div.clientHeight)) {
             // Action
         }
         else {
-            // Hide box
+            // Otherwise, hide 'box'
             this.box_div.style.display = 'none';
+            Area = 0;
         }
     }
     
-    right_click(x, y) {
+    right_click(x, y, id) {
         // Show box at position x, y
         this.x = x;
         this.y = y;
@@ -35,18 +60,27 @@ var box = class {
         this.box_div.style.left = x+'px';
         this.box_div.style.top = y+'px';
         
+        if(id === undefined) {
+            Area = 0;
+        }
+        
+        // Content according to area
         switch(Area) {
             case 0:
-                this.box_div.innerHTML = '<p>Upload a file</p><p>Upload a dir</p>';
+                this.box_div.innerHTML = '<p>'+txt.Interface.upfile+'</p><p>'+txt.Interface.updir+'</p>';
                 break;
             case 1:
-                this.box_div.innerHTML = '<p>Rename file</p><p>Remove file</p>';
+                this.box_div.innerHTML = '<p>'+txt.Interface.rdfile+'</p><p>'+txt.Interface.rnmfile+'</p><p>'+txt.Interface.rmfile+'</p><p>'+txt.Interface.fileinfo+'</p>';
+                break;
+            case 2:
+                this.box_div.innerHTML = '<p>'+txt.Interface.rddir+'</p><p>'+txt.Interface.rnmdir+'</p><p>'+txt.Interface.rmdir+'</p><p>'+txt.Interface.dirinfo+'</p>';
         }
         this.box_div.style.display = 'block';
     }
 };
 
 window.oncontextmenu = function(event) {
+    // Disable right click outside desktop div
     return false;
 }
 
@@ -56,23 +90,56 @@ window.onclick = function(event) {
 }
 
 window.onload = function() {
+    
+    // Get txt from user's language json (language.js)
+    getJSON();
+    
+    /*
+    */
+    
     Box = new box();
     
+    // Right click inside desktop div
     document.querySelector("#desktop").addEventListener("contextmenu", function(event) {
-        Box.right_click(event.clientX, event.clientY);
+        if(Area == 0) {
+            // If we are inside desktop but not inside its children
+            Box.right_click(event.clientX, event.clientY);
+        }
+        else {
+            // If we are inside its children, set Area to 0 because this function is always called when user call file's actions or folder's actions
+            // Next, we will be able to use right click inside desktop div (area = 0) and when we call file's actions or folder's actions, 'box' for 'desktop' area will not be displayed
+            Area = 0;
+        }
         return false;
     });
     
+    // Right click inside divs with file's class (these divs are children of 'desktop')
+    // After the execution of the function below, the function for 'desktop' above will be called automatically (because we are inside desktop) and will set Area to 0 without displaying a new 'box'
     // Files actions
     var files = document.querySelectorAll(".file");
     for (var i = 0; i < files.length; i++) {
-        files[i].addEventListener("mouseover", function() {
+        // For each file
+        files[i].addEventListener("contextmenu", function(event) {
+            // Right click
             Area = 1;
-        });
-        files[i].addEventListener("mouseout", function() {
-            Area = 0;
+            // Call right_click function with div's id
+            Box.right_click(event.clientX, event.clientY, this.id);
+            return false;
         });
     }
     
+    // Right click inside divs with folder's class (these divs are children of 'desktop')
+    // After the execution of the function below, the function for 'desktop' above will be called automatically (because we are inside desktop) and will set Area to 0 without displaying a new 'box'
     // Folders actions
+    var folders = document.querySelectorAll(".folder");
+    for (var i = 0; i < folders.length; i++) {
+        // For each folder
+        folders[i].addEventListener("contextmenu", function(event) {
+            // Right click
+            Area = 2;
+            // Call right_click function with div's id
+            Box.right_click(event.clientX, event.clientY, this.id);
+            return false;
+        });
+    }
 }
--- public/js/language.js
+++ public/js/language.js
@@ -1,7 +1,51 @@
+// language.js
+
+var txt; // All strings of user's language
+var lang = 'en'; // User's language
+
 function changeLanguage(lang)
 {
     var date = new Date();
     date.setTime(date.getTime()+(365*24*3600*1000));
     document.cookie = "lang="+lang+"; expires="+date.toGMTString()+"; path=/";
     window.location.reload();
+}
+
+function getLanguage() {
+    var name = "lang=";
+    var ca = document.cookie.split(';');
+    for(var i = 0; i <ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0)==' ') {
+            c = c.substring(1);
+        }
+        if (c.indexOf(name) == 0) {
+            return c.substring(name.length,c.length);
+        }
+    }
+    return "";
+}
+
+function getJSON() {
+    // Get txt from user's language json
+    
+    var clang = getLanguage();
+    if(clang != '') {
+        lang = clang;
+    }
+    
+    var xmlhttp = new XMLHttpRequest();
+    var url = "/core/public/translations/"+lang+".json";
+
+    xmlhttp.onreadystatechange = function() {
+        if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
+            setJSON(JSON.parse(xmlhttp.responseText));
+        }
+    };
+    xmlhttp.open("GET", url, true);
+    xmlhttp.send();
+}
+
+function setJSON(arr) {
+    txt = arr;
 }
