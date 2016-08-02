ff747bd452d9c087738d536d93defd89543a35b3
Super-Baleine <paul.feuvraux@protonmail.ch>
2016-08-02 23:36:41 +0200

    correction

--- library/MVC/Mail.php
+++ library/MVC/Mail.php
@@ -1,29 +1,29 @@
 <?php
 class Mail
 {
-    private $_from = "quantacloud@openmailbox.org";
+    private $_from = "muonium@protonmail.ch";
     private $_to;
     private $_subject;
     private $_message;
-    
+
     function setFrom($from) {
         $this->_from = $from;
     }
-    
+
     function setTo($to) {
         $this->_to = $to;
     }
-    
+
     function setSubject($subject) {
         $this->_subject = $subject;
     }
-    
+
     function setMessage($message) {
         $this->_message = $message;
     }
-    
+
     function send() {
-    
+
         $passage_line = "\n";
         // We filter servers that encounter bugs.
         if (!preg_match("#^[a-z0-9._-]+@(hotmail|live|msn).[a-z]{2,4}$#", $this->_to))
@@ -39,8 +39,8 @@ class Mail
         //==========
 
         //=====Create the header
-        $header = "From: \"Quanta Cloud\"<".$this->_from.">".$passage_line;
-        $header.= "Reply-to: \"Quanta Cloud\" <".$this->_from.">".$passage_line;
+        $header = "From: \"Muonium\"<".$this->_from.">".$passage_line;
+        $header.= "Reply-to: \"Muonium\" <".$this->_from.">".$passage_line;
         $header.= "MIME-Version: 1.0".$passage_line;
         $header.= "Content-Type: multipart/alternative;".$passage_line." boundary=\"$boundary\"".$passage_line;
         //==========
