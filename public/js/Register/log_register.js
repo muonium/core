1c54080a7866f61c2bccb790ab5a3c8f7c3fe6a7
Super-Baleine <paul.feuvraux@protonmail.ch>
2016-11-28 18:24:00 +0100

    implementation of the CEK #4

--- public/js/Register/log_register.js
+++ public/js/Register/log_register.js
@@ -1,31 +1,42 @@
-/*
-* @name         : sendRegisterRequest()
-* @description  : Permet l'envoi de la requête d'inscription avec les identifiants
-*/
-
-/* log_register.js:109 Uncaught ReferenceError: aeJs is not defined
-    generateKeys @ log_register.js:109
-    xhr.onreadystatechange @ log_register.js:41
-*/
-
-if('function' === typeof importScripts) {
-    importScripts("aeJs.worker.js");
-}
+//@TODO description of this js file
 
 window.onload = function() {
 
     // Get txt from user's language json (language.js)
     getJSON();
-    
+
     window.addEventListener("keydown", function(event) {
         switch(event.keyCode) {
             case 13:
                 // enter
                 sendRegisterRequest();
-                break;    
+                break;
         }
     });
 }
+
+/*
+* @name: cekGeneration()
+* @description: generation of the content encryption key,
+* CEK means "Content Encryption Key"
+*/
+var cekGeneration = function()
+{
+	var password = sjcl.random.randomWords(32);
+	var password = sjcl.codec.base64.fromBits(password);
+	var salt = sjcl.random.randomWords(32);
+	var salt = sjcl.codec.base64.fromBits(salt);
+	var contentEncryptionKey = sjcl.misc.pbkdf2(password, salt, 4096, 256); //4096 : iteration, 256 : key size
+	var contentEncryptionKey = sjcl.codec.base64.fromBits(contentEncryptionKey);
+	console.log("CEK generated!");
+	return contentEncryptionKey;
+}
+
+/*
+* @name         : sendRegisterRequest()
+* @description  : send the user's informations
+*/
+
 
 var sendRegisterRequest = function()
 {
@@ -62,7 +73,8 @@ var sendRegisterRequest = function()
                 {
                     // success message
                     if(xhr.responseText.substr(0, 3) == "ok@") {
-                        //generateKeys(field_passphrase);
+						//get the CEK
+						var cek = cekGeneration();
                         returnArea.innerHTML = xhr.responseText.substr(3);
                         window.location.href="Home";
                         return false;
@@ -75,59 +87,8 @@ var sendRegisterRequest = function()
             }
         }
 
-        xhr.send("mail="+field_mail+"&login="+field_login+"&pass="+mui_hash(field_password)+"&pass_confirm="+mui_hash(field_password_confirm)+"&passphrase="+encodeURIComponent(field_passphrase)+"&passphrase_confirm="+encodeURIComponent(field_passphrase_confirm)+"&doubleAuth="+doubleAuth); 
+        xhr.send("mail="+field_mail+"&login="+field_login+"&pass="+mui_hash(field_password)+"&pass_confirm="+mui_hash(field_password_confirm)+"&passphrase="+encodeURIComponent(field_passphrase)+"&passphrase_confirm="+encodeURIComponent(field_passphrase_confirm)+"&doubleAuth="+doubleAuth+"&cek"+cek);
         //xhr.send("mail="+field_mail+"&login="+field_login+"&pass="+mui_hash(field_password)+"&pass_confirm="+mui_hash(field_password_confirm)+"&passphrase="+mui_hash(field_passphrase)+"&passphrase_confirm="+mui_hash(field_passphrase_confirm)+"&doubleAuth="+doubleAuth);
 
     }
 }
-
-/*
-* @name         : generateKeys(string mail, string passphrase)
-* @description  : Permet la génération de la clé publique et de la clé privée à partir de la passphrase
-*/
-/*var generateKeys = function(passphrase)
-{
-    var returnArea = document.querySelector("#return p");
-    if(passphrase.length != 0)
-    {
-        aeJs.actions.encrypt("pp", passphrase);
-        //});
-        sessionStorage.setItem("passphrase", passphrase);
-    }
-    else
-    {
-        returnArea.innerHTML = "Key gen impossible.";
-    }
-}*/
-
-/*
-* @name         : openSession(string passphrase)
-* @description  : Permet la génération d'une session ainsi que le stockage en local de la passphrase
-*/
-/*var openSession = function(passphrase)
-{
-    var returnArea = document.querySelector("#return p");
-    var xhr = new XMLHttpRequest();
-    xhr.open("POST", "Inscription/GenerateSession", true);
-    xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-    xhr.onreadystatechange = function()
-    {
-        if(xhr.status == 200 && xhr.readyState == 4)
-        {
-            console.log(xhr.responseText);
-            if(xhr.responseText.length == 10) // Longueur du token
-            {
-                sessionStorage.setItem("t", xhr.responseText);
-                sessionStorage.setItem("pp",aeJs.actions.encrypt(passphrase)); //encrypt passphrase in sessionStorage
-                //sessionStorage.setItem("pp", CryptoJS.AES.encrypt(passphrase, xhr.responseText));
-                returnArea.innerHTML = "End. Redirecting...";
-                setTimeout(function(){document.location.href = "Accueil";}, 1000);
-            }
-            else
-            {
-                returnArea.innerHTML = "stockage of passphase impossible.";
-            }
-        }
-    }
-    xhr.send(null);
-}*/
