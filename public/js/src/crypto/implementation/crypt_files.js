53e9712789a47b8e288a0271a6d356c5ec6527d3
Super-Baleine <paul.feuvraux@protonmail.ch>
2016-11-28 19:45:16 +0100

    add native base64 encoding/decoding

--- public/js/src/crypto/implementation/crypt_files.js
+++ public/js/src/crypto/implementation/crypt_files.js
@@ -2,6 +2,7 @@
 * @name: fileEncryption()
 * @description: encrypt the file before uploading
 * @params: file, cek is the content encryption key
+* @dependencies: base64.js
 */
 var fileEncryption = function(file, cek)
 {
@@ -10,6 +11,7 @@ var fileEncryption = function(file, cek)
 	var cipheredFile = sjcl.encrypt(cek, file,{
 		mode:'gcm', iter:2048, ks:256, ts:128, adata:aDATA
 	});
+	var cipheredFile = base64.encode(cipheredFile);
 	return cipheredFile;
 }
 
@@ -18,9 +20,11 @@ var fileEncryption = function(file, cek)
 * @name: fileDecryption()
 * @description: decrypt the file after being downloaded
 * @params: file, cek
+* @dependencies: base64.js
 */
 var fileDecryption = function(file, cek)
 {
+	var decryptedFile = base64.decode(file);
 	var decryptedFile = sjcl.decrypt(cek, file);
 	return decryptedFile;
 }
