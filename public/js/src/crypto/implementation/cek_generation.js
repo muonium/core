e903f649bfd4efb1c02655314e4e769dea8cbbc8
Super-Baleine <paul.feuvraux@protonmail.ch>
2016-12-01 21:45:01 +0100

    add the crypto-details guide for random devs who'd like to know how works our system

--- public/js/src/crypto/implementation/cek_generation.js
+++ public/js/src/crypto/implementation/cek_generation.js
@@ -4,12 +4,15 @@
 * @params: 'passphrase' is the user's passphrase or secrete password, call this like you want to...
 * CEK means "Content Encryption Key"
 * the CEK is encrypted by the passphrase of the user
+* @note: the CEK is just a random secrete password,
+* and pbkdf2 internally in SJCL derivate a key from the CEK
 * @dependencies: crypt_cek.js
 */
 var cekGeneration = function(passphrase)
 {
 	var contentEncryptionKey = sjcl.random.randomWords(4); //256 bits key size
 	var contentEncryptionKey = sjcl.codec.base64.fromBits(contentEncryptionKey);
-	console.log("CEK generated!");
+	console.log("CEK generated!\n\
+	More details about the cryptography implementation at https://github.com/muonium/core/wiki/Crypto-details");
 	return cekEncryption(passphrase, contentEncryptionKey); //cekEncryption() come from js/implementation/crypt_cek.js
 }
