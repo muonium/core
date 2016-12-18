ab89300bb4b9fc894197e87d19a5272378e807e9
Super-Baleine <paul.feuvraux@muonium.ch>
2016-12-18 22:59:34 +0100

    cek generation complete (normally)

--- public/js/src/crypto/gen_cek.js
+++ public/js/src/crypto/gen_cek.js
@@ -1,5 +1,14 @@
-var genCek = function(){
+var genCek = {};
+genCek.plt = function(){
 	var key = sjcl.random.randomWords(4); //get a random value
 	var key = sjcl.codec.base64.fromBits(key); //the base64 encoding will be the CEK itself
+	return key;
+}
+
+genCek.enc = function(key, passphrase){
+	var a = sjcl.random.randomWords(1);
+	//encrypt it
+	var key = sjcl.encrypt(passphrase, key, {mode:'gcm', iter:2000, ks:256, a, ts:128});
+	var key = base64.encode(key); //don't store a Json in mongoDB...
 	return key;
 }
