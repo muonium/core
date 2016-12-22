c17939f994d6b71b4e2044132b41ca2363c38de2
Super-Baleine <paul.feuvraux@muonium.ch>
2016-12-22 23:23:42 +0100

    cek stored when the user log in

--- public/js/Login/log_connect.js
+++ public/js/Login/log_connect.js
@@ -14,13 +14,13 @@ window.onload = function() {
 
     // Get txt from user's language json (language.js)
     getJSON();
-    
+
     window.addEventListener("keydown", function(event) {
         switch(event.keyCode) {
             case 13:
                 // enter
                 sendConnectionRequest();
-                break;    
+                break;
         }
     });
 }
@@ -34,7 +34,7 @@ var sendConnectionRequest = function()
     var returnArea = document.querySelector("#return");
 
     returnArea.innerHTML = "<img src='./public/pictures/index/loader.gif' style='height: 3vh;' />";
-    
+
     if(field_password.length < 6 || field_passphrase.length < 1 || field_username.length < 3)
         returnArea.innerHTML = txt.Register.form;
     else {
@@ -53,11 +53,12 @@ var sendConnectionRequest = function()
                 {
                     // success message
                     if(xhr.responseText.substr(0, 3) == "ok@") {
-                        window.location.href="Home";
-                        return false;
+						decryptCek(field_passphrase);
+						return false;
                     }
                     else if(xhr.responseText.substr(0, 3) == "va@") {
-                        window.location.href="Validate";
+						decryptCek(field_passphrase);
+						tempTransfer(cek)
                         return false;
                     }
                     else {
@@ -68,103 +69,34 @@ var sendConnectionRequest = function()
             }
         }
 
-       xhr.send("username="+encodeURIComponent(field_username)+"&pass="+mui_hash(field_password)+"&passphrase="+encodeURIComponent(field_passphrase)); 
-       // xhr.send("username="+encodeURIComponent(field_username)+"&pass="+mui_hash(field_password)+"&passphrase="+mui_hash(field_passphrase));
+       xhr.send("username="+encodeURIComponent(field_username)+"&pass="+mui_hash(field_password));
     }
 }
-/*
-* @name         : getKeys(string passphrase)
-* @description  : Permet la récupération des clés privée et publique
-*/
-/*var getKey = function(passphrase)
-{
-    var returnArea = document.querySelector("#return");
-    var status_key;
-    var xhr_private_key = new XMLHttpRequest();
-    xhr_public.open("POST", "Connexion/getPrivateKey", true);
-    //xhr_public.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-    xhr_public.onreadystatechange = function()
-    {
-        if(xhr_private_key.status == 200 && xhr_public.readyState == 4)
-        {
-            if(xhr_private_key.responseText == "")
-            {
-              alert("Private key cannot be recieved");
-              status_key = 0;
-            }
-            else
-            {
-                sessionStorage.setItem("pp",aeJs.actions.encrypt(xhr_private_key.responseText));
-                status_key = 1;
-            }
-        }
-    }
-    xhr_private_key.send(null);
-    var checkKeysStatus = setInterval(function(){
-        if(status_key == 1)
-        {
-            clearInterval(checkKeysStatus);
-            aeJs.actions.encrypt(passphrase);
-        }
-    }, 500);
-}*/
 
-/*
-* @name         : encryptPassphrase(string passphrase)
-* @description  : Permet de chiffrer le mot de passe de la clé privée et de le stocker en local
-*/
-/*var encryptPassphrase = function(passphrase)
-{
-    var returnArea = document.querySelector("#return");
-    var xhr = new XMLHttpRequest();
-    xhr.open("POST", "Connexion/getToken", true);
-    xhr.onreadystatechange = function()
-    {
-        if(xhr.status == 200 && xhr.readyState == 4)
-        {
-            if((xhr.responseText).length == 128)
-            {
-                sessionStorage.setItem("token", CryptoJS.AES.encrypt(passphrase, xhr.responseText));
-                returnArea.innerHTML = "<p class='success'>Authentification terminée. Redirection en cours...</p>";
-                setTimeout(function(){
-                    openSession();
-                }, 1000);
-            }
-            else
-            {
-                returnArea.innerHTML = "<p class='error'>Erreur lors de l'authentification. Veuillez réessayer ultérieurement</p>";
-                setTimeout(function(){
-                    destroySession();
-                }, 1000);
-            }
-        }
-    }
-    xhr.send(null);
-}*/
+//requires sjcl.js and base64.js
+var decryptCek = function(kek){
+	var xhr = new XMLHttpRequest();
+	xhr.open("GET", "Login/GetCek", true)
+	xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
 
-/*
-* @name         : openSession()
-* @description  : Ouverture de la session
-*/
-/*var openSession = function()
-{
-    document.location.href = "Accueil";
-}*/
+	xhr.onreadystatechange = function(){
+		if (xhr.status == 200 && xhr.readyState == 4) {
+			var cek = xhr.responseText;
+		}
+	}
+	xhr.send(null);
 
-/*
-* @name         : destroySession()
-* @description  : Destruction de la session
-*/
-/*var destroySession = function()
-{
-    var xhr = new XMLHttpRequest();
-    xhr.open("POST", "Connexion/destroySession", true);
-    xhr.onreadystatechange = function()
-    {
-        if(xhr.status == 200 && xhr.readyState == 4)
-        {
-            document.location.href = "Accueil";
-        }
-    }
-    xhr.send(null);
-}*/
+	var cek = base64.decode(cek);
+	testCek(kek, cek);
+	sessionStorage.setItem("kek", kek);
+	window.location.href="Home";
+}
+
+var testCek = function(kek, cek){
+	try {
+		var dec = sjcl.decrypt(kek, cek);
+		sessionStorage.setItem("cek", cek);
+	} catch (e) {
+		returnArea.innerHTML = "<p>Your passphrase is invalid!</p>";
+	}
+}
