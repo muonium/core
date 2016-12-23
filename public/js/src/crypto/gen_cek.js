6b4a183939afe22f8662553447570f79a667788d
Super-Baleine <paul.feuvraux@muonium.ch>
2016-12-23 20:03:01 +0100

    keep the CEK as a bytes array

--- public/js/src/crypto/gen_cek.js
+++ public/js/src/crypto/gen_cek.js
@@ -1,7 +1,6 @@
 var genCek = {};
 genCek.plt = function(){
 	var key = sjcl.random.randomWords(4); //get a random value
-	var key = sjcl.codec.base64.fromBits(key); //the base64 encoding will be the CEK itself
 	return key;
 }
 
