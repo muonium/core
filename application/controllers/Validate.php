5ce0cb7331887cbc9148f8ccfd1a111d7e0188d4
Super-Baleine <paul.feuvraux@muonium.ch>
2017-01-02 14:10:49 +0100

    logout once account valided

--- application/controllers/Validate.php
+++ application/controllers/Validate.php
@@ -4,31 +4,31 @@ use \library\MVC as l;
 use \application\models as m;
 
 class Validate extends l\Languages {
-        
+
         private $id_user;
         private $val_key;
         private $err_msg;
-        
+
         private $_modelUser;
         private $_modelUserVal;
         private $_mail;
-        
+
         function DefaultAction() {
             require_once(DIR_VIEW."vSendMail.php");
         }
-        
+
         function KeyAction($id_user, $key) {
             if(!is_numeric($id_user) || strlen($key) < 128)
                 exit(header('Location: '.MVC_ROOT.'/Error/Error/404'));
             $this->id_user = $id_user;
             $this->val_key = $key;
-            
+
             $this->_modelUserVal = new m\UserValidation();
             $this->_modelUserVal->id_user = $this->id_user;
-            
+
             if(!($this->_modelUserVal->getKey())) // Unable to find key
                 exit(header('Location: '.MVC_ROOT.'/Error/Error/404'));
-            
+
             if($this->_modelUserVal->getKey() != $this->val_key) {
                 // Different key, send a new mail ?
                 $this->err_msg = $this->txt->Validate->message;
@@ -40,16 +40,18 @@ class Validate extends l\Languages {
                 $_SESSION['id'] = $this->id_user;
                 if(!empty($_SESSION['validate']))
                     unset($_SESSION['validate']);
-                header('Location: '.MVC_ROOT);
+				//all is good, his account is validated
+				//now we disconnect him, he have to log in himself
+                header('Location: '.MVC_ROOT.'Logout');
             }
         }
-        
+
         function SendMailAction() {
         // Send AGAIN registration mail with validation key
             sleep(1000);
             if(!empty($_SESSION['id'])) {
                 // If logged
-                
+
                 // One mail per minute
                 $w = 0;
                 if(!empty($_SESSION['sendMail'])) {
@@ -83,7 +85,7 @@ class Validate extends l\Languages {
                     $this->_mail->_message = str_replace("[id_user]", $_SESSION['id'], str_replace("[key]", $key, $this->txt->Register->message));
                     $this->_mail->send();
                     $_SESSION['sendMail'] = time();
-                    
+
                     $this->err_msg = $this->txt->Global->mail_sent;
                     require_once(DIR_VIEW."vValidate.php");
                 }
