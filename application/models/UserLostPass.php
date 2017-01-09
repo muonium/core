4f870a898bfedb0c95d3d322cfbcb084a7a986bc
hitoshi54 <dylanclement7@gmail.com>
2017-01-09 21:12:09 +0100

    Quick fix (update user lostpass expire when sending a mail again)

--- application/models/UserLostPass.php
+++ application/models/UserLostPass.php
@@ -3,26 +3,26 @@ namespace application\models;
 use \library\MVC as l;
 
 class UserLostPass extends l\Model {
-        
+
         /*
             1   id                  int(11)         AUTO_INCREMENT
             2   id_user             int(11)
             3   val_key             varchar(128)
             4   expire              int(11)
         */
-        
+
         protected $id;
         protected $id_user;
         protected $val_key;
         protected $expire;
-        
+
         /* ******************** SETTER ******************** */
-        
+
         /* ******************** GETTER ******************** */
         function getIdUser() {
             return $this->id_user;
         }
-        
+
         function getKey() {
             $req = self::$_sql->prepare("SELECT val_key FROM user_lostpass WHERE id_user = ?");
             $req->execute(array($this->id_user));
@@ -31,7 +31,7 @@ class UserLostPass extends l\Model {
             $res = $req->fetch();
             return $res['val_key'];
         }
-        
+
         function getExpire() {
             $req = self::$_sql->prepare("SELECT expire FROM user_lostpass WHERE id_user = ?");
             $req->execute(array($this->id_user));
@@ -40,22 +40,21 @@ class UserLostPass extends l\Model {
             $res = $req->fetch();
             return $res['expire'];
         }
-        
+
         /* **************************************** */
-        
+
         function Delete() {
             $req = self::$_sql->prepare("DELETE FROM user_lostpass WHERE id_user = ?");
             return $req->execute(array($this->id_user));
         }
-        
+
         function Insert() {
             $req = self::$_sql->prepare("INSERT INTO user_lostpass VALUES (NULL, ?, ?, ?)");
             return $req->execute(array($this->id_user, $this->val_key, $this->expire));
         }
-        
+
         function Update() {
-            $req = self::$_sql->prepare("UPDATE user_lostpass SET val_key = ? WHERE id_user = ?");
-            return $req->execute(array($this->val_key, $this->id_user));
+            $req = self::$_sql->prepare("UPDATE user_lostpass SET val_key = ?, expire = ? WHERE id_user = ?");
+            return $req->execute(array($this->val_key, $this->expire, $this->id_user));
         }
     }
-
