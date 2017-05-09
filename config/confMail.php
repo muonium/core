9c835ca26e67d916a0b9f932ce36cb660c360195
hitoshi54 <dylanclement7@gmail.com>
2017-05-09 18:50:33 +0200

    Implemented PHPMailer library for sending mails

---
+++ config/confMail.php
@@ -0,0 +1,10 @@
+<?php
+namespace config;
+// Class used for sending mails configuration
+class confMail {
+	const smtp_host = "smtp.x.x";
+	const user = "muonium@x.x";
+	const password = "password";
+	const smtp_secure = "tls"; // tls or ssl
+	const port = 587;
+};
--- library/MVC/Mail.php
+++ library/MVC/Mail.php
@@ -1,59 +1,48 @@
 <?php
 namespace library\MVC;
+use \config as conf;
+
+require_once(ROOT.'/library/PHPMailer/class.phpmailer.php');
+require_once(ROOT.'/library/PHPMailer/class.smtp.php');
 
 class Mail
 {
-    protected $_from = "muonium@muonium.ch";
     protected $_to;
     protected $_subject;
     protected $_message;
-    
+
     function __set($attr, $val) {
         $this->$attr = $val;
     }
 
     function send() {
-
         $passage_line = "\n";
         // We filter servers that encounter bugs.
-        if (!preg_match("#^[a-z0-9._-]+@(hotmail|live|msn).[a-z]{2,4}$#", $this->_to))
+        if (!preg_match("#^[a-z0-9._-]+@(hotmail|live|msn).[a-z]{2,4}$#", $this->_to)) {
             $passage_line = "\r\n";
+        }
 
-        //=====Message in txt format and in HTML format
+        // Message in txt format and in HTML format
         $message_txt = strip_tags(str_replace("<br />", $passage_line, str_replace("<br>", $passage_line, $this->_message)));
         $message_html = $this->_message;
-        //==========
 
-        //=====Create the boundary
-        $boundary = "-----=".md5(rand());
-        //==========
+        $mail = new \PHPMailer;
+        //$mail->SMTPDebug = 3; // Debug
+        $mail->isSMTP();
+        $mail->Host = conf\confMail::smtp_host;
+        $mail->SMTPAuth = true;
+        $mail->Username = conf\confMail::user;
+        $mail->Password = conf\confMail::password;
+        $mail->SMTPSecure = conf\confMail::smtp_secure;
+        $mail->Port = conf\confMail::port;
 
-        //=====Create the header
-        $header = "From: \"Muonium\"<".$this->_from.">".$passage_line;
-        $header.= "Reply-to: \"Muonium\" <".$this->_from.">".$passage_line;
-        $header.= "MIME-Version: 1.0".$passage_line;
-        $header.= "Content-Type: multipart/alternative;".$passage_line." boundary=\"$boundary\"".$passage_line;
-        //==========
+        $mail->setFrom($this->_from, $this->_fromName);
+        $mail->addAddress($this->_to);
+        $mail->isHTML(true);
+        $mail->Subject = $this->_subject;
+        $mail->Body    = $message_html;
+        $mail->AltBody = $message_txt;
 
-        //=====Create the message
-        $message = $passage_line."--".$boundary.$passage_line;
-        //=====Add txt formatted message
-        $message.= "Content-Type: text/plain; charset=\"utf-8\"".$passage_line;
-        $message.= "Content-Transfer-Encoding: 8bit".$passage_line;
-        $message.= $passage_line.$message_txt.$passage_line;
-        //==========
-        $message.= $passage_line."--".$boundary.$passage_line;
-        //=====Add HTML formatted message
-        $message.= "Content-Type: text/html; charset=\"utf-8\"".$passage_line;
-        $message.= "Content-Transfer-Encoding: 8bit".$passage_line;
-        $message.= $passage_line.$message_html.$passage_line;
-        //==========
-        $message.= $passage_line."--".$boundary."--".$passage_line;
-        $message.= $passage_line."--".$boundary."--".$passage_line;
-        //==========
-
-        //=====Send the mail
-        mail($this->_to, $this->_subject, $message, $header);
+        $mail->send();
     }
 };
-
