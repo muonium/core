2b4f5f8642de72ded9074d3c5854a447e2f8a1ee
Super-Baleine <paul.feuvraux@muonium.ch>
2017-12-25 13:34:21 +0100

    version update
    Signed-off-by: Super-Baleine <paul.feuvraux@muonium.ch>

--- index.php
+++ index.php
@@ -8,7 +8,7 @@ require_once("./config/autoload.php");
 // Defines
 
 // Mui Version
-define('VERSION', '2017.11.9.0');
+define('VERSION', '2017.12.25.0');
 
 define('DS', DIRECTORY_SEPARATOR);
 define('ROOT', __DIR__);
