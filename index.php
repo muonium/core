9645278eef9bb116f871fa9300316c933e17e292
Paul Feuvraux <paul@muonium.ee>
2018-01-01 16:21:10 +0100

    happy new year :tada:

--- index.php
+++ index.php
@@ -8,7 +8,7 @@ require_once("./config/autoload.php");
 // Defines
 
 // Mui Version
-define('VERSION', '2017.12.25.0');
+define('VERSION', '2018.01.01.0');
 
 define('DS', DIRECTORY_SEPARATOR);
 define('ROOT', __DIR__);
