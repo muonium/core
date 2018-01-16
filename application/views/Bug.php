481a1c6a7b1315faf74b3da7b76c9e4f467460c2
hitoshi54 <dylanclement7@gmail.com>
2018-01-16 22:56:54 +0100

    Send bug page

--- application/views/Bug.php
+++ application/views/Bug.php
@@ -4,53 +4,52 @@
     $_t = new l\Template(self::$txt->Global->bug);
 
     $_t->addCss([
-		'blue/blue',
-    	'blue/container',
-    	'blue/header',
-    	'blue/inputs',
-    	'blue/menu',
-    	'blue/section-large-content'
+		'2018/style'
 	])->addJs(['Bug', 'check']);
 
 	echo $_t->getHead();
 	echo $_t->getHeader();
+	echo $_t->getSidebar();
 ?>
-    <div id="container">
-        <section id="large-content">
+    <div class="container-large">
+		<form method="post" action="<?php echo MVC_ROOT; ?>/Bug/Form" class="form-bug">
             <h1><?php echo self::$txt->Global->bug; ?></h1>
 
             <p><strong><?php if(!empty($this->_message)) { echo_h($this->_message); } ?></strong></p>
 
-            <form method="post" action="<?php echo MVC_ROOT; ?>/Bug/Form" class="block">
-                <p>
-                    <label for="os"><?php echo self::$txt->Bug->os; ?>* :</label>
+            <fieldset>
+                <legend><?php echo self::$txt->Bug->os; ?>*</legend>
+				<p class="input-large">
                     <select name="os" id="os" required>
                         <option value="">-------</option>
                         <?php $this->printValues('os'); ?>
                     </select>
-                </p>
+				</p>
+            </fieldset>
 
-                <p>
-                    <label for="browser"><?php echo self::$txt->Bug->browser; ?>* :</label>
+			<fieldset>
+				<legend><?php echo self::$txt->Bug->browser; ?>*</legend>
+                <p class="input-large">
                     <select name="browser" id="browser" required>
                         <option value="">-------</option>
                         <?php $this->printValues('browser'); ?>
                     </select>
                 </p>
+			</fieldset>
 
-                <p>
-                    <label for="browserVersion"><?php echo self::$txt->Bug->browserVersion; ?> :</label>
-                    <input type="text" name="browserVersion" id="browserVersion" value="<?php if(!empty($_POST['browserVersion'])) { echo_h($_POST['browserVersion']); } ?>">
-                </p>
+            <p class="input-large">
+                <input type="text" name="browserVersion" id="browserVersion" value="<?php if(!empty($_POST['browserVersion'])) { echo_h($_POST['browserVersion']); } ?>" placeholder="<?php echo self::$txt->Bug->browserVersion; ?>">
+            </p>
 
-                <p>
-                    <label for="message"><?php echo self::$txt->Bug->message; ?>* :</label>
+			<fieldset>
+				<legend><?php echo self::$txt->Bug->message; ?>*</legend>
+                <p class="input-large">
                     <textarea name="message" id="message" cols="50" rows="5"><?php if(!empty($_POST['message'])) { echo_h($_POST['message']); } ?></textarea>
                 </p>
+			</fieldset>
 
-				<input type="submit" value="OK">
-            </form>
-        </section>
+			<input type="submit" value="<?php echo self::$txt->Bug->send; ?>" class="btn">
+        </form>
     </div>
 <?php
     echo $_t->getFooter();
