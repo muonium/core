fb3dbec4362d7559a6c6c6b060f9e2acd16b54d8
Super-Baleine <paul.feuvraux@muonium.ch>
2018-02-04 16:48:13 +0100

    increment version

--- index.php
+++ index.php
@@ -8,7 +8,7 @@ require_once("./config/autoload.php");
 // Defines
 
 // Mui Version
-define('VERSION', '2018.02.04.0');
+define('VERSION', '2018.02.04.1');
 
 define('DS', DIRECTORY_SEPARATOR);
 define('ROOT', __DIR__);
